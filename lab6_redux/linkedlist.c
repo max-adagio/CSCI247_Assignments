b77fd918d1b6c0a889f6ca59531fa92d6e0a95eb
widjajm <widjajm@wwu.edu>
2023-05-01 20:34:04 -0700

    Wassup
    dealing with some weird github stuff
    i think the lab6 directory is completely broken, because i accidently
    made it a repo earlier and i can't figure out how to totally undo that

---
+++ lab6_redux/linkedlist.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int value;
+    struct node* next;
+    struct node* previous;
+} node_t;
+// size of 4 + 8 + 8 = 20B
+
+node_t* make_node(int value, node_t* next, node_t* previous){
+    
+    node_t* newNode = (node_t*) malloc(sizeof(node_t));
+    newNode->value = value;
+    newNode->next = next;
+    newNode->previous = previous;
+
+    return newNode;
+
+    // node_t node = make_node(value, next, previous);
+}
+
+node_t* add_back(node_t** list, int value) {
+    if ((*list) == NULL) {
+        node_t* node = make_node(value, NULL, NULL); 
+        
+        printf("- added successsfully\n");
+        return node; 
+        
+    } else {
+        // find the last element of the list
+        printf("- started else\n");
+
+        node_t* i = *list;
+        while (i->next != NULL) {
+            i = i->next;
+            printf("- loop\n");
+        }
+        printf("- passed iterator\n");
+        // now i, the node_t, contains the last node in the list, where i-> == NULL
+        node_t* newNode = make_node(value, NULL, i);  
+        i->next = newNode;
+
+        printf("- ended else\n");
+        return newNode;
+    }
+}
+
+void print_list(node_t* list, int backwards) {
+    while (list != NULL) {
+        printf("%d ", list->value);
+        list = list->next;
+    }
+    printf("\n");
+}
+
+
+int main() {
+    node_t* bruh = NULL;
+    printf("- test01\n");
+
+    bruh = add_back(&bruh, 5);
+    print_list(bruh, 0);
+    bruh = add_back(&bruh, 8);
+    print_list(bruh, 0);
+    bruh = add_back(&bruh, 12);
+    print_list(bruh, 0);
+
+
+
+}
